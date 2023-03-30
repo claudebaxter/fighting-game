8b30a2c5b6afa6fcdcace78cabe4a8203811cb3f
elborracho420 <e.baxter@dark-coin.io>
2023-03-30 17:32:03 -0500

    Fixed missing ;, set up main game animation function / loop animate() and triggered loop.

--- index.js
+++ index.js
@@ -8,26 +8,33 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
     constructor(position) {
-        this.position = position
+        this.position = position;
     }
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150);
     }
-}
+};
 
 const player = new Sprite({
     x: 0,
     y: 0
-})
+});
 
 player.draw();
 
 const enemy = new Sprite({
     x: 400,
     y: 100
-})
+});
 
-enemy.draw()
+enemy.draw();
 
 console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    console.log('animate')
+}
+
+animate();
