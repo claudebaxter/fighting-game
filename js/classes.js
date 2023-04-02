11e7365a91e4e74d7eb153b7b0293e8fa3579ea9
elborracho420 <e.baxter@dark-coin.io>
2023-04-01 19:38:59 -0500

    Animated shop sprite by iterating through image frame by frame.

--- js/classes.js
+++ js/classes.js
@@ -1,18 +1,35 @@
 class Sprite {
     //pass through arguments to constructor as a single object
     //so no order is required nor is either argument
-    constructor({position, imageSrc}) {
+    constructor({position, imageSrc, scale = 1, framesMax = 1}) {
         this.position = position;
         this.width = 50;
         this.height = 150;
         this.image = new Image();
         this.image.src = imageSrc;
+        this.scale = scale;
+        this.framesMax = framesMax;
+        this.framesCurrent = 0;
     }
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.drawImage(
+                this.image, 
+                this.framesCurrent * (this.image.width / this.framesMax), 
+                0,
+                this.image.width / this.framesMax,
+                this.image.height,
+                this.position.x, 
+                this.position.y, 
+                (this.image.width / this.framesMax) * this.scale, 
+                this.image.height * this.scale)
     }
     update() {
         this.draw();
+        if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++
+        } else {
+            this.framesCurrent = 0;
+        }
         
     }
 };
