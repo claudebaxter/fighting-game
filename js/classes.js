a62891fc80adad132a9ebf00153f423be8411ed2
elborracho420 <e.baxter@dark-coin.io>
2023-04-01 19:54:18 -0500

    Improving the speed and appearance of the shop sprite animation.

--- js/classes.js
+++ js/classes.js
@@ -10,6 +10,8 @@ class Sprite {
         this.scale = scale;
         this.framesMax = framesMax;
         this.framesCurrent = 0;
+        this.framesElapsed = 0;
+        this.framesHold = 5; //animation speed
     }
     draw() {
         c.drawImage(
@@ -25,12 +27,15 @@ class Sprite {
     }
     update() {
         this.draw();
-        if (this.framesCurrent < this.framesMax - 1) {
-        this.framesCurrent++
-        } else {
-            this.framesCurrent = 0;
-        }
-        
+        this.framesElapsed++
+
+        if (this.framesElapsed % this.framesHold === 0) {
+            if (this.framesCurrent < this.framesMax - 1) {
+                this.framesCurrent++
+            } else {
+                this.framesCurrent = 0;
+            }
+        }  
     }
 };
 
